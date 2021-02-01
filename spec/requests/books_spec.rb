81c999bb583c000a4f7c73a43317ffa9e47b5814
Jocelyn Lee <biheyeblue@gmail.com>
2021-02-01 18:13:14 +0800

    commit

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -2,33 +2,36 @@ require 'rails_helper'
 
 describe 'Books API', type: :request do
   describe " GET /books" do  
-      it 'returns all books' do
-         FactoryBot.create(:book, title: '1984', author: 'George Orwell')
-        #  FactoryBot.create(:book, title: 'The Catcher in the Rye', author: 'J. D. Salinger')
+    it 'returns all books' do
+      FactoryBot.create(:book, title: 'AAA', author: 'AAA')
+      # FactoryBot.create(:book, title: 'The Catcher in the Rye', author: 'J. D. Salinger')
 
-         get '/api/v1/books'
+     get '/api/v1/books'
+      expect(response).to have_http_status(:success) 
+      expect(JSON.parse(response.body).size).to eq(2)
     
-         expect(response).to have_http_status(:success) 
-         expect(JSON.parse(response.body).size).to eq(2)
-      end
+    end
   end
     
   describe 'POST /books' do
     it 'create a new book' do
-
-        post '/api/v1/books', params: { book: {title:'The Hunger Games', author:'Suzanne Collins'}  }
-        
+      expect{  
+        post '/api/v1/books', params: { book: {title:'BBB', author:'BBB'}  }
+      }.to change { Book.count }.from(0).to(1)
         expect(response).to have_http_status(:created)  
     end 
   end
 
-  describe 'DELETE/ books/:id' do
-    let(:book){FactoryBot.create(:book, title: '1984', author: 'George Orwell')}
+  describe 'DELETE /books/:id' do
+    let!(:book){FactoryBot.create(:book, title: 'CCC', author: 'CCC')}
 
     it 'deletes a book' do
-      delete "/api/v1/books/#{book.id}"
+      expect{
+      delete "/api/v1/books/#{book.id}" 
+      }.to change { Book.count }.from(1).to(0)
         
         expect(response).to have_http_status(:no_content)  
     end 
   end
+
 end
